cba0f9fc44d9dec8762a8535a1b002e58340629f
Michaela <mi-and@outlook.com>
2024-08-12 21:05:00 +0200

    added styling

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -20,15 +20,21 @@ function TodoForm({ onSubmit }: Props) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className="w-full flex flex-col items-center justify-center gap-2"
+    >
       <input
         type="text"
         placeholder="Skriv en sak.."
         value={text}
         onChange={(e) => setText(e.target.value)}
-        className="p-2 border border-gray-300 rounded bg-gray-100 w-full"
+        className=" p-2 border border-gray-300 rounded w-full bg-gray-100 md:w-1/2"
       />
-      <button type="submit" className="p-2 bg-blue-500 text-white rounded">
+      <button
+        type="submit"
+        className="p-2 bg-blue-500 text-white rounded  w-1/4"
+      >
         Spara
       </button>
     </form>
