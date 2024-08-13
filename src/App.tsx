58b4493c7f3c8d4be45b7e4d13d2eca54178b304
Michaela <mi-and@outlook.com>
2024-08-13 14:33:01 +0200

    add day and time in header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import DeleteButton from "./components/DeleteButton/DeleteButton";
 import EditTodo from "./components/EditButton/EditButton";
 import TodoForm from "./components/TodoForm/TodoForm";
+import Header from "./Header";
 
 interface Todo {
   id: number;
@@ -60,64 +61,67 @@ function App() {
   };
 
   return (
-    <div className="bg-[url('https://wallpapers.com/images/high/white-marble-background-5w2vm7osht89hx9s.webp')] bg-cover bg-center bg-opacity-50 w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl md:text-6xl mb-10">Att-göra-lista</h1>
-      <div className="flex flex-col w-full p-2 md:w-1/3 gap-4 ">
-        <div className="flex flex-col items-center bg-white p-4 w-full rounded-md shadow-xl">
-          <TodoForm onSubmit={handleAddTodo} />
-        </div>
-        <div className="flex flex-col bg-white p-4 w-full rounded-md shadow-xl">
-          <h2 className="font-bold">Att göra:</h2>
-          <hr className="mb-4" />
-          {todos.length === 0 ? (
-            <p>Listan är tom</p>
-          ) : (
-            <div className="flex flex-col gap-4 mb-4">
-              {todos.map((todo) => (
-                <div
-                  key={todo.id}
-                  className="p-2 flex items-center bg-gray-200 justify-between shadow-xl rounded-md hover:bg-gray-300"
-                >
-                  {editingTodoId === todo.id ? (
-                    <EditTodo
-                      todo={todo.text}
-                      onSave={handleSaveEdit}
-                      onCancel={handleCancelEdit}
-                    />
-                  ) : (
-                    <>
-                      <span className="">{todo.text}</span>
-                      <div className="flex gap-2">
-                        <button
-                          onClick={() => handleEdit(todo.id)}
-                          className="p-2 text-black rounded"
-                          data-testid="edit-button"
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="size-6"
+    <>
+      <Header />
+      <div className="bg-[url('https://wallpapers.com/images/high/white-marble-background-5w2vm7osht89hx9s.webp')] bg-cover bg-center bg-opacity-50 w-full h-screen flex flex-col items-center justify-center">
+        <h1 className="text-4xl md:text-6xl mb-10">Att-göra-lista</h1>
+        <div className="flex flex-col w-full p-2 md:w-1/3 gap-4 ">
+          <div className="flex flex-col items-center bg-white p-4 w-full rounded-md shadow-xl">
+            <TodoForm onSubmit={handleAddTodo} />
+          </div>
+          <div className="flex flex-col bg-white p-4 w-full rounded-md shadow-xl">
+            <h2 className="font-bold">Att göra:</h2>
+            <hr className="mb-4" />
+            {todos.length === 0 ? (
+              <p>Listan är tom</p>
+            ) : (
+              <div className="flex flex-col gap-4 mb-4">
+                {todos.map((todo) => (
+                  <div
+                    key={todo.id}
+                    className="p-2 flex items-center bg-gray-200 justify-between shadow-xl rounded-md hover:bg-gray-300"
+                  >
+                    {editingTodoId === todo.id ? (
+                      <EditTodo
+                        todo={todo.text}
+                        onSave={handleSaveEdit}
+                        onCancel={handleCancelEdit}
+                      />
+                    ) : (
+                      <>
+                        <span className="">{todo.text}</span>
+                        <div className="flex gap-2">
+                          <button
+                            onClick={() => handleEdit(todo.id)}
+                            className="p-2 text-black rounded"
+                            data-testid="edit-button"
                           >
-                            <path
-                              strokeLinejoin="round"
-                              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                            />
-                          </svg>
-                        </button>
-                        <DeleteButton onClick={() => handleDelete(todo.id)} />
-                      </div>
-                    </>
-                  )}
-                </div>
-              ))}
-            </div>
-          )}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-6"
+                            >
+                              <path
+                                strokeLinejoin="round"
+                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                              />
+                            </svg>
+                          </button>
+                          <DeleteButton onClick={() => handleDelete(todo.id)} />
+                        </div>
+                      </>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function Header() {
+  const today = new Date();
+  const [currentTime, setCurrentTime] = useState<string>("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const formattedTime = now.toLocaleTimeString("sv-SE", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+      setCurrentTime(formattedTime);
+    };
+
+    updateTime(); // Uppdatera direkt
+    const intervalId = setInterval(updateTime, 1000); // Uppdatera varje sekund
+
+    return () => clearInterval(intervalId); // Rensa intervallet när komponenten avmonteras
+  }, []);
+
+  const formattedDate = today.toLocaleDateString("sv-SE", {
+    weekday: "long", // "måndag"
+    year: "numeric", // "2023"
+    month: "long", // "augusti"
+    day: "numeric", // "13"
+  });
+  return (
+    <div className="flex flex-col p-4">
+      <div className="flex justify-end">
+        <h2 className="text-4xl">{currentTime}</h2>
+      </div>
+      <div className="flex justify-end">
+        <p className="text-2xl">{formattedDate}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
